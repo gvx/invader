327636eb1663438d0b462d6be4e4aa97104d5440
Robin Wellner <gyvox.public@gmail.com>
2011-07-15 13:11:31 +0200

    Adds MIT/X11 license

--- credits.lua
+++ credits.lua
@@ -14,17 +14,19 @@ local function c(t)
 	return {text = t, t = 0, i = i, x = 100, y = -100*i, centery = -250*i, targety = 150*i, update = update}
 end
 Credits.font = love.graphics.newFont('kabel.ttf', 20)
+Credits.license_font = love.graphics.newFont('kabel.ttf', 12)
 Credits.credits = {c"game by Robin Wellner", c"Kabel font from KDE", c"uses Stateful by kikito"}
+Credits.license = love.filesystem.read("LICENSE")
 
 function Credits:enterState()
-	love.graphics.setColor(255,255,255)
-	love.graphics.setFont(Credits.font)
+	alpha = 0
 end
 function Credits:exitState()
 	love.graphics.setFont(MainMenu.font)
 end
 
 function Credits:update(dt)
+	alpha = math.min(alpha + dt * (5 + alpha), 255)
 	for i, credit in ipairs(Credits.credits) do
 		credit:update(dt)
 	end
@@ -32,9 +34,14 @@ function Credits:update(dt)
 end
 
 function Credits:draw()
+	love.graphics.setFont(Credits.font)
+	love.graphics.setColor(255,255,255)
 	for i, credit in ipairs(Credits.credits) do
 		love.graphics.print(credit.text, credit.x, credit.y)
 	end
+	love.graphics.setColor(255,255,255, alpha)
+	love.graphics.setFont(Credits.license_font)
+	love.graphics.print(Credits.license, 420, 130)
 end
 
 function Credits:keypressed(k, u)
